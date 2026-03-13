54108c0955db024478194df14e544cffb2c57d44
José Alexandre dos Santos <santos.822070@gmail.com>
2026-03-13 13:00:21 -0300

    implementandoDicas

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 
 FILE* abrir(char nome[], char modo[]);
      FILE* fecharArg(FILE *arg);
+     void adicionarDicas();
 
 
 int main() {
@@ -11,6 +15,27 @@ int main() {
     temp=fecharArg(temp);
     return 0;
 }
+void adicionarDicas() {
+    char dica[201];
+    char resposta;
+    bool validacao=false;
+    do{  
+            system("cls");
+            printf("Digite uma dica de até 200 caracteres: \n");
+            scanf("%200[^\n]", dica);
+            printf("Deseja salvar a dica? (s/n): ");
+            scanf(" %c", &resposta);
+    }while(resposta!='s' && resposta!='n');
+
+    FILE *arg;
+    arg=abrir("dicas.txt","a");
+    if(arg!=NULL) {
+        fprintf(arg, "%s\n", dica);
+        fecharArg(arg);
+    }
+    
+}   
+
 FILE* abrir(char nome[], char modo[]) {
     FILE *arg;
     arg=fopen(nome,modo);
